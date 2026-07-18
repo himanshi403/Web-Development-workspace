c22a9c291a4ad387ce0967155edadf90e18bb728
himanshi <prasharhimanshi75@gmail.com>
2026-07-18 14:19:11 +0530

    Added stats of jobStatus and improved css

--- Full-Stack-Project/Job-Application-Tracker/script.js
+++ Full-Stack-Project/Job-Application-Tracker/script.js
@@ -19,6 +19,16 @@ document.getElementById("search");
 const filter =
 document.getElementById("filter");
 
+const totalJobs=document.getElementById("totalJobs");
+
+const appliedJobs=document.getElementById("appliedJobs");
+
+const interviewJobs=document.getElementById("interviewJobs");
+
+const offerJobs=document.getElementById("offerJobs");
+
+const rejectedJobs=document.getElementById("rejectedJobs");
+
 let jobs=[];
 searchInput.addEventListener("input",displayJobs);
 
@@ -41,6 +51,7 @@ function addJob(){
     
 
     const job={
+        id:Date.now(), //every click gets differ no.
         company,
         role,
         status
@@ -60,6 +71,17 @@ function addJob(){
     //display funcn
     function displayJobs(){
         jobList.innerHTML="";
+
+        //if there are no added jobs
+        if(jobs.length===0){
+
+jobList.innerHTML="<h2>No Applications Yet</h2>";
+
+updateStats();
+
+return;
+
+}
         const status=filter.value;
         const keyword=searchInput.value.toLowerCase();
         
@@ -68,27 +90,74 @@ function addJob(){
             const statusMatch=status==="All" || job.status===status;
             return companyMatch && statusMatch;
         }).forEach(function(job,index){
-            jobList.innerHTML+=`
-            <div class="job">
-            <h3>${job.company}</h3>
-            <p>Role:${job.role}</p>
-            <p>Status:${job.status}</p>
-            <button onclick="deleteJob(${index})">Delete</button>
+            jobList.innerHTML += `<div class="job">
+                                  <div class="header">
+
+                                <h3>${job.company}</h3>
+
+                              <span class="status">${job.status}</span>
+
+                               </div>
+
+               <p><strong>Role:</strong> ${job.role}</p>
+
+                                <div class="buttons">
+
+                         <button onclick="editJob(${job.id})">
+                                          Edit
+                      </button>
+
+                  <button onclick="deleteJob(${job.id})">
+                                  Delete
+                 </button>
+
+               </div>
+
             </div>
-            `;
+                `;
         
         })
+        
+        updateStats();
     }
 
-    function deleteJob(index){
-        jobs.splice(index,1);
+    function deleteJob(id){
+        jobs = jobs.filter(job => job.id !== id);
         localStorage.setItem("jobs", JSON.stringify(jobs));
         displayJobs();
     }
+    //to edit anything in particular job card
+
+    function editJob(id){
+
+        const job=jobs.find(function(j){
+            return j.id===id;
+        });
+         companyInput.value = job.company;
+    roleInput.value = job.role;
+    statusInput.value = job.status;
+
+    deleteJob(id);
+}
 
     const savedJobs=localStorage.getItem("jobs");
     if(savedJobs){
         jobs=JSON.parse(savedJobs);
         displayJobs();
+    }
+
+    //to fill the stats
+
+    function updateStats(){
+        totalJobs.textContent=jobs.length;
+
+        appliedJobs.textContent=jobs.filter(job=>job.status==="Applied").length;
+
+        interviewJobs.textContent=jobs.filter(job=>job.status==="Interview").length;
+
+        offerJobs.textContent=jobs.filter(job=>job.status==="Offer").length;
+
+        rejectedJobs.textContent=jobs.filter(job=>job.status==="Rejected").length;
+
     }
     
